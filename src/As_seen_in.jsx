de368f2d1f359107c0785a23f5dcc0f908047b62
john.lin <>
2025-09-02 11:35:15 +0800

    更新專案程式碼

--- src/As_seen_in.jsx
+++ src/As_seen_in.jsx
@@ -14,9 +14,44 @@ export default function As_seen_in() {
                     <ul>
                         <li>
                             <a href=''>
-                                <div className='as_seen_in_li_title'></div>
-                                <div className='as_seen_in_li_p'></div>
-                                <div className='as_seen_in_li_button'></div>
+                                <div className='as_seen_in_li_title'><img src="https://cdn.prod.website-files.com/63c3059a658a4c786dbd48fd/664da0c9bfb9c61658d7a07c_Screenshot%202024-05-20%20at%2023.16%201%40svg.svg" loading="lazy" alt="" class="tesimonials_logo"/></div>
+                                <div className='as_seen_in_li_p'>“Our Text-To-AR App Got 25K Users Within Two Months Of Launch”</div>
+                                <div className='as_seen_in_li_button'>Read more</div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href=''>
+                                <div className='as_seen_in_li_title'><img src="https://cdn.prod.website-files.com/63c3059a658a4c786dbd48fd/666817918b9a702c504d6bf0_image%20102.png" loading="lazy" alt="" sizes="100vw" srcset="https://cdn.prod.website-files.com/63c3059a658a4c786dbd48fd/666817918b9a702c504d6bf0_image%20102-p-500.png 500w, https://cdn.prod.website-files.com/63c3059a658a4c786dbd48fd/666817918b9a702c504d6bf0_image%20102.png 780w" class="tesimonials_logo"/></div>
+                                <div className='as_seen_in_li_p'>“Shader app: Creation AR magic and video with founder, Darya Sesitskaya”</div>
+                                <div className='as_seen_in_li_button'>Read more</div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href=''>
+                                <div className='as_seen_in_li_title'><img src="https://cdn.prod.website-files.com/63c3059a658a4c786dbd48fd/663dea48eb3f5b4d3474d5b6_product-hunt-logo-horizontal-black%201.svg" loading="lazy" alt="" class="tesimonials_logo"/></div>
+                                <div className='as_seen_in_li_p'><img src="https://cdn.prod.website-files.com/63c3059a658a4c786dbd48fd/663de561c0df05452046bf02_product%20of%20the%20day.svg" loading="lazy" alt="" class="nav_logo"/></div>
+                                <div className='as_seen_in_li_button'>Read more</div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href=''>
+                                <div className='as_seen_in_li_title'><img src="https://cdn.prod.website-files.com/63c3059a658a4c786dbd48fd/663dea48b88c315e0f54f4db_TechCrunch%201.svg" loading="lazy" alt="" class="tesimonials_logo"/></div>
+                                <div className='as_seen_in_li_p'>“Former Snap design lead debuts Shader, an AR creation tool that uses AI to generate custom effects“</div>
+                                <div className='as_seen_in_li_button'>Read more</div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href=''>
+                                <div className='as_seen_in_li_title'><img src="https://cdn.prod.website-files.com/63c3059a658a4c786dbd48fd/663dea49bc64494c1b4d6f0b_image%2090.svg" loading="lazy" alt="" class="tesimonials_logo"/></div>
+                                <div className='as_seen_in_li_p'>“Transforming text to AR: Backed by Betaworks and Greycroft, Shader’s AI-driven real-time camera app”</div>
+                                <div className='as_seen_in_li_button'>Read more</div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href=''>
+                                <div className='as_seen_in_li_title'><img src="https://cdn.prod.website-files.com/63c3059a658a4c786dbd48fd/664a37c2b4eba08fddc369ae_REVIEW%204.svg" loading="lazy" alt="" class="tesimonials_logo"/></div>
+                                <div className='as_seen_in_li_p'><img src="https://cdn.prod.website-files.com/63c3059a658a4c786dbd48fd/663de561c0df05452046bf02_product%20of%20the%20day.svg" loading="lazy" alt="" class="nav_logo"/></div>
+                                <div className='as_seen_in_li_button'>Read more</div>
                             </a>
                         </li>
                     </ul>
