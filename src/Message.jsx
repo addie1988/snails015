13a92a87329e3c402810f565ddafd04d932ae05b
john.lin <>
2025-09-02 10:14:54 +0800

    備份

--- src/Message.jsx
+++ src/Message.jsx
@@ -15,101 +15,118 @@ export default function Message() {
           <div className="message_content_area_content">
             <ul>
               <li>
-                <h3>Sooo cool!</h3>
-                <div className="score">
-                  <span>★</span>
-                  <span>★</span>
-                  <span>★</span>
-                  <span>★</span>
-                  <span>★</span>
+                <div className="li_box">
+                  <h3>Sooo cool!</h3>
+                  <div className="score">
+                    <span>★</span>
+                    <span>★</span>
+                    <span>★</span>
+                    <span>★</span>
+                    <span>★</span>
+                  </div>
+                  <p>
+                    Shader is a blast to use! It's so much fun and easy! I
+                    started editing my pics, and I was enthralled, stuck for 2
+                    hours :-) Great Job!
+                  </p>
                 </div>
-                <p>
-                  Shader is a blast to use! It's so much fun and easy! I started
-                  editing my pics, and I was enthralled, stuck for 2 hours :-)
-                  Great Job!
-                </p>
                 <span>Sagewire</span>
               </li>
               <li>
-                <h3>Fun for all Ages</h3>
-                <div className="score">
-                  <span>★</span>
-                  <span>★</span>
-                  <span>★</span>
-                  <span>★</span>
-                  <span>★</span>
+                <div className="li_box">
+                  <h3>Fun for all Ages</h3>
+                  <div className="score">
+                    <span>★</span>
+                    <span>★</span>
+                    <span>★</span>
+                    <span>★</span>
+                    <span>★</span>
+                  </div>
+                  <p>
+                    This app is a fantastic find! It's incredibly easy to use,
+                    making it good for both kids and adults alike. The interface
+                    is intuitive and well designed.
+                    <br></br>
+                    <br></br>
+                    Highly recommended for anyone looking to have fun with
+                    augmented reality!
+                  </p>
                 </div>
-                <p>
-                  This app is a fantastic find! It's incredibly easy to use,
-                  making it good for both kids and adults alike. The interface
-                  is intuitive and well designed.
-                  <br />
-                  Highly recommended for anyone looking to have fun with
-                  augmented reality!
-                </p>
                 <span>Rulik Tech</span>
               </li>
               <li>
-                <h3>So fun</h3>
-                <div className="score">
-                  <span>★</span>
-                  <span>★</span>
-                  <span>★</span>
-                  <span>★</span>
-                  <span>★</span>
+                <div className="li_box">
+                  <h3>So fun</h3>
+                  <div className="score">
+                    <span>★</span>
+                    <span>★</span>
+                    <span>★</span>
+                    <span>★</span>
+                    <span>★</span>
+                  </div>
+                  <p>
+                    Ever wanted to be a princess superhero/ your favorite
+                    character? This app can take you to the wonderland! So fun,
+                    clever and easy to use. My niece loves it!
+                  </p>
                 </div>
-                <p>
-                  Ever wanted to be a princess superhero/ your favorite
-                  character? This app can take you to the wonderland! So fun,
-                  clever and easy to use. My niece loves it!
-                </p>
                 <span>Christinity</span>
               </li>
               <li>
-                <h3>Pretty cool app</h3>
-                <div className="score">
-                  <span>★</span>
-                  <span>★</span>
-                  <span>★</span>
-                  <span>★</span>
-                  <span>★</span>
+                <div className="li_box">
+                  <h3>Pretty cool app</h3>
+                  <div className="score">
+                    <span>★</span>
+                    <span>★</span>
+                    <span>★</span>
+                    <span>★</span>
+                    <span>★</span>
+                  </div>
+                  <p>
+                    Asked Shader to add Spiderman costume to my dog's picture.
+                    Did a great job! Looking forward to create more with it.
+                  </p>
                 </div>
-                <p>
-                  Asked Shader to add Spiderman costume to my dog's picture. Did
-                  a great job! Looking forward to create more with it.
-                </p>
                 <span>Rishi Jash</span>
               </li>
               <li>
-                <h3>Fun app</h3>
-                <div className="score">
-                  <span>★</span>
-                  <span>★</span>
-                  <span>★</span>
-                  <span>★</span>
-                  <span>★</span>
+                <div className="li_box">
+                  <h3>Fun app</h3>
+                  <div className="score">
+                    <span>★</span>
+                    <span>★</span>
+                    <span>★</span>
+                    <span>★</span>
+                    <span>★</span>
+                  </div>
+                  <p>Super fun to try different Al effects</p>
                 </div>
-                <p>Super fun to try different Al effects</p>
                 <span>RoHs</span>
               </li>
               <li>
-                <h3>Love it for the holidays!</h3>
-                <div className="score">
-                  <span>★</span>
-                  <span>★</span>
-                  <span>★</span>
-                  <span>★</span>
-                  <span>★</span>
+                <div className="li_box">
+                  <h3>Love it for the holidays!</h3>
+                  <div className="score">
+                    <span>★</span>
+                    <span>★</span>
+                    <span>★</span>
+                    <span>★</span>
+                    <span>★</span>
+                  </div>
+                  <p>
+                    I randomly find this app on Instagram just before Christmas
+                    and loved it so much! Was using it for a Christmas scene and
+                    sending pic to my friends. Love it.
+                  </p>
                 </div>
-                <p>I randomly find this app on Instagram just before Christmas and loved it so much! Was using it for a Christmas scene and sending pic to my friends. Love it.</p>
                 <span>Marina</span>
               </li>
             </ul>
 
             <div className="message_button">
-                <div className="message_button_content">
-                    <a href="">Join our Discord</a>
-                </div>
+              <div className="message_button_content">
+                <a href="">Join our Discord</a>
+              </div>
             </div>
           </div>
         </div>
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/snails015/',   // 這裡要跟 repo 名稱一樣
 })
